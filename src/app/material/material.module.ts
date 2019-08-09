f8ccc0e30f918499899fd43a4adf4f0cc0229f3e
Ivaylo Dimitrov <ivaylo.dimitrov@ad.mentormate.bg>
2019-08-09 20:23:56 +0300

    environment set

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 
-import {
-  MatInputModule,
-  MatFormFieldModule,
-  MatRadioModule,
-  MatSelectModule,
-  MatOptionModule,
-  MatButtonModule,
-  MatCheckboxModule,
-  MatDialogModule,
-  MatSnackBarModule,
-  MatProgressSpinnerModule,
-  MAT_SNACK_BAR_DEFAULT_OPTIONS
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule, MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 
 const modules = [
   MatInputModule,
